52c35d89c88b0fc5e8bf91375ea572f708b4cbef
Mui Nguyen <88954743+miuiunguyen@users.noreply.github.com>
2021-08-28 08:55:04 +0700

    Update: add range in loop

--- Day-5-100days-of-Coding/learn code/loop.py
+++ Day-5-100days-of-Coding/learn code/loop.py
@@ -3,3 +3,12 @@ for fruit in fruits:
     print(fruit)
     print(fruit + " Pie")
 print(fruits)
+
+#For loop with range
+#for number in range(1, 11, 3):
+ #   print(number)
+
+total = 0
+for number in range(1, 101):
+    total += number
+print(total)
